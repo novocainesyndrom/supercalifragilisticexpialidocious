dec331d8e460e437f688ac7d3c2826fdb2ad5b7d
novocainesyndrom <prostoemail97@gmail.com>
2020-02-29 01:14:17 +0200

    Delete files

--- apiread.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = 'https://reqres.in/api/users'
-resp = requests.get(url)
-resp.raise_for_status()
-apiData = resp.json()
-emlList = [email['email'] for email in apiData['data']]
-for email in emlList:
-	print(email)
